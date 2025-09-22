147053cc0b51fb11ef3c2f6e9c2418049d389be7
Timo Lobitz <timo.lobitz@gmail.com>
2025-09-23 00:09:46 +0200

    nix devenv

---
+++ devenv.nix
@@ -0,0 +1,53 @@
+{ pkgs, lib, config, inputs, devenv-zsh, ... }:
+
+{
+	imports = [ devenv-zsh.plugin ];
+	zsh.enable = true;
+
+	# https://devenv.sh/basics/
+	env.GREET = "edo";
+
+	# https://devenv.sh/packages/
+	packages = [
+		pkgs.git
+		pkgs.premake5
+		pkgs.curl
+		pkgs.zip
+		pkgs.libGLU
+		#pkgs.vcpkg
+		pkgs.nlohmann_json
+		pkgs.sqlite
+		pkgs.libevent
+		pkgs.libgit2
+		pkgs.fmt
+		pkgs.glfw
+		pkgs.glm
+		pkgs.irrlicht
+
+	];
+
+	# https://devenv.sh/languages/
+	# languages.rust.enable = true;
+	languages.c.enable = true;
+	languages.cplusplus.enable = true;
+
+	# https://devenv.sh/processes/
+	# processes.cargo-watch.exec = "cargo-watch";
+
+	# https://devenv.sh/services/
+	# services.postgres.enable = true;
+
+	# https://devenv.sh/scripts/
+
+	# https://devenv.sh/tasks/
+	# tasks = {
+	#   "myproj:setup".exec = "mytool build";
+	#   "devenv:enterShell".after = [ "myproj:setup" ];
+	# };
+
+	# https://devenv.sh/tests/
+	# https://devenv.sh/git-hooks/
+	# git-hooks.hooks.shellcheck.enable = true;
+
+	# See full reference at https://devenv.sh/reference/options/
+}
